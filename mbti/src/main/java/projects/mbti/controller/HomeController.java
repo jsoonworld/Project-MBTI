f70973bacdd64e1052645ed27c2484541c13cade
권장순 <jsoonworld@gmail.com>
2023-05-14 21:28:56 +0900

    login/api/setting

--- mbti/src/main/java/projects/mbti/controller/HomeController.java
+++ mbti/src/main/java/projects/mbti/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class HomeController {
 
-    @RequestMapping("/")
+    // @RequestMapping("/")
     public String home() {
         log.info("home controller");
         return "home";
---
+++ mbti/src/main/java/projects/mbti/login/Login.java
@@ -0,0 +1,9 @@
+package projects.mbti.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Login {
+    private String data;
+}
